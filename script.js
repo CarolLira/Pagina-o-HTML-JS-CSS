2974f35cc8715a2c65f195e97f0911d6e4626ef6
CarolLira <carol017lira@gmail.com>
2021-08-21 18:03:21 -0300

    criando paginação para itens da pagina

---
+++ script.js
@@ -0,0 +1,137 @@
+const data = Array.from({ length: 100 })
+    .map((item, i) => `Item ${(i + 1)}`);
+
+let perPage = 5;
+const state = {
+    page: 1,
+    perPage,
+    totalPage: Math.ceil(data.length / perPage),
+    maxVisibleButtons: 5,
+};
+
+const html = {
+    get(element) {
+        return document.querySelector(element);
+    }
+};
+
+const controls = {
+    next() {
+        state.page++;
+        const afterLastPage = state.page > state.totalPage;
+        if (afterLastPage) {
+            state.page--;
+        }
+    },
+    prev() {
+        state.page--;
+        const beforeFirstPage = state.page < 1;
+        if (beforeFirstPage) {
+            state.page++;
+        }
+    },
+    goTo(page) {
+        page = parseInt(page);
+        if (page > state.totalPage) {
+            state.page = state.totalPage;
+        } else if (page < 1) {
+            state.page = 1;
+        } else {
+            state.page = page;
+        }
+    },
+    createListeners() {
+        html.get('.first').addEventListener('click', () => {
+            controls.goTo(1);
+            update();
+        });
+        html.get('.last').addEventListener('click', () => {
+            controls.goTo(state.totalPage);
+            update();
+        });
+        html.get('.prev').addEventListener('click', () => {
+            controls.prev();
+            update();
+        });
+        html.get('.next').addEventListener('click', () => {
+            controls.next();
+            update();
+        });
+    }
+};
+
+const list = {
+    create(item) {
+        const div = document.createElement('div');
+        div.classList.add('item');
+        div.innerHTML = item;
+
+        html.get('.list').appendChild(div);
+    },
+    update() {
+        html.get('.list').innerHTML = "";
+        let page = state.page - 1;
+        let start = page * state.perPage;
+        let end = start + state.perPage;
+
+        const paginatedItems = data.slice(start, end);
+        paginatedItems.forEach(list.create);
+    }
+};
+
+const buttons = {
+    element: html.get('#paginate .numbers'),
+    create(number) {
+        const button = document.createElement('div');
+        button.innerHTML = number;
+
+        if (state.page == number) {
+            button.classList.add('active');
+        }
+
+        button.addEventListener('click', (event) => {
+            const page = event.target.innerText;
+            controls.goTo(page);
+            update();
+        });
+
+        buttons.element.appendChild(button);
+    },
+    update() {
+        buttons.element.innerHTML = "";
+        const { maxLeft, maxRight } = buttons.calculateMaxVisible();
+
+        for (let page = maxLeft; page <= maxRight; page++) {
+            buttons.create(page);
+        }
+    },
+    calculateMaxVisible() {
+        const { maxVisibleButtons } = state;
+        let maxLeft = (state.page - Math.floor(maxVisibleButtons / 2));
+        let maxRight = (state.page + Math.floor(maxVisibleButtons / 2));
+        if (maxLeft < 1) {
+            maxLeft = 1;
+            maxRight = maxVisibleButtons;
+        }
+
+        if (maxRight > state.totalPage) {
+            maxLeft = state.totalPage - (maxVisibleButtons - 1);
+            maxRight = state.totalPage;
+
+            if (maxLeft < 1) maxLeft = 1;
+        }
+        return { maxLeft, maxRight };
+    }
+};
+
+function update() {
+    buttons.update();
+    list.update();
+}
+
+function init() {
+    update();
+    controls.createListeners();
+}
+
+init();
